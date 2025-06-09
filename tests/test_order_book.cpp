3645c18c39897b02c80492578985184607d7358d
Muhammed A. Choudhary <muhammed.choudhary@student.manchester.ac.uk>
2025-06-09 18:55:26 +0100

    Implemented a test case to check that orders are cancelled properly

--- tests/test_order_book.cpp
+++ tests/test_order_book.cpp
@@ -113,3 +113,35 @@ TEST(OrderBookTest, ValidBestSell)
 
     EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order.getOrderId());
 }
+
+TEST(OrderBookTest, ValidCancelOrder)
+{
+    OrderBook book;
+
+    Order buy_order_1(Side::BUY, 100, 12);
+    Order buy_order_2(Side::BUY, 90, 23);
+
+    Order sell_order_1(Side::SELL, 200, 23);
+    Order sell_order_2(Side::SELL, 201, 32);
+
+    book.addOrder(buy_order_1);
+    book.addOrder(buy_order_2);
+    book.addOrder(sell_order_1);
+    book.addOrder(sell_order_2);
+
+    ASSERT_NE(book.getBestBuy(), nullptr);
+
+    book.cancelOrder(book.getBestBuy()->getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order_2.getOrderId());
+
+    book.cancelOrder(book.getBestSell()->getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order_2.getOrderId());
+
+    EXPECT_NO_THROW(book.cancelOrder(1111));
+
+    book.cancelOrder(book.getBestBuy()->getOrderId());
+    book.cancelOrder(book.getBestSell()->getOrderId());
+
+    EXPECT_EQ(book.getBestBuy(), nullptr);
+    EXPECT_EQ(book.getBestSell(), nullptr);
+}
