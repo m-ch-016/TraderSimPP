1cd4ba7a1615be049ead7f5f52ccc336432f9df5
Muhammed A. Choudhary <muhammed.choudhary@student.manchester.ac.uk>
2025-06-11 15:23:30 +0100

    Created a Trade struct file

---
+++ include/Trade.h
@@ -0,0 +1,12 @@
+#ifndef TRADE_H
+#define TRADE_H
+
+#include <cstdint>
+
+struct Trade {
+    std::uint64_t buyerID;
+    std::uint64_t sellerID;
+    std::uint64_t price;
+    std::uint64_t quantity;
+};
+#endif
