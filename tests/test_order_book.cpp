2ce9bee6bd486e821710cc4cfe2688d04cb8767d
Muhammed A. Choudhary <muhammed.choudhary@student.manchester.ac.uk>
2025-06-11 17:00:54 +0100

    Recorrected test cases to work properly with the smart pointers

--- tests/test_order_book.cpp
+++ tests/test_order_book.cpp
@@ -4,8 +4,8 @@
 TEST(OrderBookTest, ValidAddOrder)
 {
     OrderBook book;
-    Order order(Side::BUY, 20, 100);
-    book.addOrder(&order);
+    std::unique_ptr<Order> order = std::make_unique<Order>(Side::BUY, 20, 100);
+    book.addOrder(std::move(order));
 
     ASSERT_NE(book.getBestBuy(), nullptr);
     EXPECT_EQ(book.getBestBuy()->getSide(), Side::BUY);
@@ -19,11 +19,11 @@ TEST(OrderBookTest, ValidSingleOrderMatch)
 {
     OrderBook book;
 
-    Order buyOrder(Side::BUY, 34, 123);
-    Order sellOrder(Side::SELL, 23, 120);
+    std::unique_ptr<Order> buyOrder = std::make_unique<Order>(Side::BUY, 34, 123);
+    std::unique_ptr<Order> sellOrder = std::make_unique<Order>(Side::SELL, 23, 120);
 
-    book.addOrder(&buyOrder);
-    book.addOrder(&sellOrder);
+    book.addOrder(std::move(buyOrder));
+    book.addOrder(std::move(sellOrder));
 
     ASSERT_NE(book.getBestBuy(), nullptr);
     ASSERT_NE(book.getBestSell(), nullptr);
@@ -41,44 +41,50 @@ TEST(OrderBookTest, ValidSequentialOrderMatch)
 {
     OrderBook book;
 
-    Order buy_order_1(Side::BUY, 50, 100);  
-    Order buy_order_2(Side::BUY, 48, 90);   
-
+    std::unique_ptr<Order> buy_order_1 = std::make_unique<Order>(Side::BUY, 50, 100);  
+    std::unique_ptr<Order> buy_order_2 = std::make_unique<Order>(Side::BUY, 48, 90);   
     
-    Order sell_order_1(Side::SELL, 45, 80); 
-    Order sell_order_2(Side::SELL, 47, 70); 
-    Order sell_order_3(Side::SELL, 49, 60); 
-
     
-    book.addOrder(&buy_order_1);  
-    book.addOrder(&buy_order_2);
+    std::unique_ptr<Order> sell_order_1 = std::make_unique<Order>(Side::SELL, 45, 80);
+    std::unique_ptr<Order> sell_order_2 = std::make_unique<Order>(Side::SELL, 47, 70); 
+    std::unique_ptr<Order> sell_order_3 = std::make_unique<Order>(Side::SELL, 49, 60); 
+    
+    auto buy_1_id = buy_order_1->getOrderId();
+    auto buy_2_id = buy_order_2->getOrderId();
+    
+    book.addOrder(std::move(buy_order_1));  
+    book.addOrder(std::move(buy_order_2));
+    
+    auto sell_1_id = sell_order_1->getOrderId();
+    auto sell_2_id = sell_order_2->getOrderId();
+    auto sell_3_id = sell_order_3->getOrderId();
 
-    book.addOrder(&sell_order_1); 
-    book.addOrder(&sell_order_2); 
-    book.addOrder(&sell_order_3); 
+    book.addOrder(std::move(sell_order_1)); 
+    book.addOrder(std::move(sell_order_2)); 
+    book.addOrder(std::move(sell_order_3)); 
 
     
     
     EXPECT_NO_THROW(book.matchOrders());
 
     ASSERT_NE(book.getBestBuy(), nullptr);
-    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order_2.getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_2_id);
     EXPECT_EQ(book.getBestBuy()->getQuantity(), 40);
 
     ASSERT_NE(book.getBestSell(), nullptr);
-    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order_3.getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_3_id);
     EXPECT_EQ(book.getBestSell()->getQuantity(), 60);
 
     EXPECT_NO_THROW(book.matchOrders());
-    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order_2.getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_2_id);
     EXPECT_EQ(book.getBestBuy()->getQuantity(), 40);
-    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order_3.getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_3_id);
     EXPECT_EQ(book.getBestSell()->getQuantity(), 60);
 
     EXPECT_NO_THROW(book.matchOrders());
-    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order_2.getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_2_id);
     EXPECT_EQ(book.getBestBuy()->getQuantity(), 40);
-    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order_3.getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_3_id);
     EXPECT_EQ(book.getBestSell()->getQuantity(), 60);
 }
 
@@ -88,10 +94,12 @@ TEST(OrderBookTest, ValidBestBuy)
 
     ASSERT_EQ(book.getBestBuy(), nullptr);
 
-    Order buy_order(Side::BUY, 123, 12);
-    book.addOrder(&buy_order);
+    std::unique_ptr buy_order = std::make_unique<Order>(Side::BUY, 123, 12);
+    auto buy_id = buy_order->getOrderId();
+    
+    book.addOrder(std::move(buy_order));
 
-    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order.getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_id);
 }
 
 TEST(OrderBookTest, ValidBestSell)
@@ -100,34 +108,43 @@ TEST(OrderBookTest, ValidBestSell)
 
     ASSERT_EQ(book.getBestSell(), nullptr);
 
-    Order sell_order(Side::SELL, 123, 123);
-    book.addOrder(&sell_order);
+    std::unique_ptr sell_order = std::make_unique<Order>(Side::SELL, 123, 123);
+    auto sell_id = sell_order->getOrderId();
+    
+    book.addOrder(std::move(sell_order));
 
-    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order.getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_id);
 }
 
 TEST(OrderBookTest, ValidCancelOrder)
 {
     OrderBook book;
 
-    Order buy_order_1(Side::BUY, 100, 12);
-    Order buy_order_2(Side::BUY, 90, 23);
+    std::unique_ptr buy_order_1 = std::make_unique<Order>(Side::BUY, 100, 12);
+    std::unique_ptr buy_order_2 = std::make_unique<Order>(Side::BUY, 90, 23);
 
-    Order sell_order_1(Side::SELL, 200, 23);
-    Order sell_order_2(Side::SELL, 201, 32);
+    std::unique_ptr sell_order_1 = std::make_unique<Order>(Side::SELL, 200, 23);
+    std::unique_ptr sell_order_2 = std::make_unique<Order>(Side::SELL, 201, 32);
 
-    book.addOrder(&buy_order_1);
-    book.addOrder(&buy_order_2);
-    book.addOrder(&sell_order_1);
-    book.addOrder(&sell_order_2);
+    auto buy_1_id = buy_order_1->getOrderId();
+    auto buy_2_id = buy_order_2->getOrderId();
+
+    auto sell_1_id = sell_order_1->getOrderId();
+    auto sell_2_id = sell_order_2->getOrderId();
+    
+
+    book.addOrder(std::move(buy_order_1));
+    book.addOrder(std::move(buy_order_2));
+    book.addOrder(std::move(sell_order_1));
+    book.addOrder(std::move(sell_order_2));
 
     ASSERT_NE(book.getBestBuy(), nullptr);
 
     book.cancelOrder(book.getBestBuy()->getOrderId());
-    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_order_2.getOrderId());
+    EXPECT_EQ(book.getBestBuy()->getOrderId(), buy_2_id);
 
     book.cancelOrder(book.getBestSell()->getOrderId());
-    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_order_2.getOrderId());
+    EXPECT_EQ(book.getBestSell()->getOrderId(), sell_2_id);
 
     EXPECT_NO_THROW(book.cancelOrder(1111));
 
